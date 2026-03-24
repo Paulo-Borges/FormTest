e906234f38473c5e7c47edd5ef7cb59e831a42e7
Borges <borgespaulo72@yahoo.com.br>
2026-03-24 13:50:43 -0300

    refactor: refatorando os input´s

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,37 @@
 import { useState } from "react";
 
+interface FormData {
+  email: string;
+  nome: string;
+}
+
+const formFields: {
+  label: string;
+  name: keyof FormData;
+  type: string;
+}[] = [
+  {
+    label: "Email",
+    name: "email",
+    type: "email",
+  },
+  {
+    label: "Nome",
+    name: "nome",
+    type: "text",
+  },
+];
+
 export function ControlledForm() {
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormData>({
     email: "",
     nome: "",
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { id, value } = e.target;
+    const { name, value } = e.target;
     // Atualiza apenas o campo que mudou, mantendo os outros
-    setFormData({ ...formData, [id]: value });
+    setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -22,28 +44,22 @@ export function ControlledForm() {
       onSubmit={handleSubmit}
       className="flex flex-col gap-5 w-screen max-w-5xl px-4"
     >
-      <div className="flex flex-col">
-        <label htmlFor="email">Email:</label>
-        <input
-          id="email"
-          type="email"
-          value={formData.email}
-          className="border"
-          //   onChange={(e) => setFormData {}
-          onChange={handleChange}
-        />
+      <div className="flex flex-col gap-4">
+        {formFields.map((field) => (
+          <div key={field.name} className="flex flex-col gap-4">
+            <label htmlFor={field.label}>{field.label}</label>
+            <input
+              name={field.name}
+              type={field.type}
+              value={formData[field.name]}
+              className="border"
+              //   onChange={(e) => setFormData {}
+              onChange={handleChange}
+            />
+          </div>
+        ))}
       </div>
-      <div className="flex flex-col ">
-        <label htmlFor="nome">Nome:</label>
-        <input
-          id="nome"
-          type="nome"
-          value={formData.nome}
-          className="border"
-          //   onChange={(e) => setFormData(e.target.value)}
-          onChange={handleChange}
-        />
-      </div>
+
       <button type="submit" className="">
         Enviar
       </button>
